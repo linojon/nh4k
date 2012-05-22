1f0479306de6f5b46bea2e87d55631ee7a96c495
Jonathan Linowes <linojon@gmail.com>
2012-05-21 21:55:45 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nh4k::Application.config.secret_token = '78fae6de730333f6b4577a2da1336afbebf4880e0b709f5b3a970878b37cdf9607c9f4e7a560d5c85fa86203dafef9d4a94601b736b907c4c2d9923aaf4cf5ba'
