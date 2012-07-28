b0ebe28c26e16c6e6883fd30017212499c69acdc
Jonathan Linowes <linojon@gmail.com>
2012-07-27 23:54:06 -0400

    attr_accessible is_nh4k

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,5 +1,5 @@
 class Mountain < ActiveRecord::Base
-  attr_accessible :amc_link, :elevation, :fullname, :lat, :lng, :name, :netc_link, :notes, :trailsnh_link
+  attr_accessible :amc_link, :elevation, :fullname, :lat, :lng, :name, :netc_link, :notes, :trailsnh_link, :is_nh4k
   
   has_many :ascents
   has_many :trips
