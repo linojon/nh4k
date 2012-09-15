83a204ba70c6029488aba654b65bf4274004ec86
Jonathan Linowes <linojon@gmail.com>
2012-09-15 15:36:55 -0400

    number the mountains

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -2,7 +2,7 @@ class Mountain < ActiveRecord::Base
   attr_accessible :amc_link, :elevation, :fullname, :lat, :lng, :name, :netc_link, :notes, :trailsnh_link, :is_nh4k
   
   has_many :ascents
-  has_many :trips
+  has_many :trips, through: :ascents
   has_many :hikers, through: :trips
   
   def fullname
